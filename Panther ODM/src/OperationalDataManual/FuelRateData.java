3916b498d2d5e10c94f9340e9ba8c7db40144cce
MikeTa <MikeTa@MikeTa-LT001.datacom.net.nz>
2017-09-05 15:39:40 +1200

    Running from data in file

--- Panther ODM/src/OperationalDataManual/FuelRateData.java
+++ Panther ODM/src/OperationalDataManual/FuelRateData.java
@@ -1,19 +1,64 @@
 package OperationalDataManual;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FuelRateData {
     private int acHeight;
     private int acSpeed;
     private int acWeight;
     private double fuelUsagePerMin;
+    private Boolean validData;
 
+    @Deprecated
     public FuelRateData(double height, double speed, double weight, double fuelPerMin) {
         acHeight = correctHeight(height);
         acSpeed = correctSpeed(speed);
         acWeight = correctWeight(weight);
+        validData = true;
 
         fuelUsagePerMin = fuelPerMin;
     }
+    
+    public FuelRateData(String lineData) {
+        double height = 0.0, speed = 0.0, weight = 0.0, fuelPerMin = 0.0;
+        validData = true;
+        
+        String[] fuelData = lineData.split("\\|");
+        if (fuelData.length == 5) {
+            height = convertStringNumber(fuelData[1]);
+            speed = convertStringNumber(fuelData[2]);
+            weight = convertStringNumber(fuelData[3]);
+            fuelPerMin = convertStringNumber(fuelData[4]);
+        } else {
+            validData = false;
+        }
 
+        if ( validData.booleanValue() && height >= 0.0 && speed >= 400.0 && weight >= 6000.0 && fuelPerMin > 0.0)
+        {
+            acHeight = correctHeight(height);
+            acSpeed = correctSpeed(speed);
+            acWeight = correctWeight(weight);
+            fuelUsagePerMin = fuelPerMin;       
+        }
+        else
+        {
+            validData = false;
+        }
+    }
+
+    private double convertStringNumber(String strNum) {
+        double retValue;
+        
+        try {
+            retValue = Double.parseDouble(strNum);
+        } catch (NumberFormatException e) {
+            validData = false;
+            return -1.0;
+        }
+        return retValue;
+    }
+    
     public boolean dataMatch(double height, double speed, double weight) {
         boolean matchVal;
 
@@ -25,6 +70,10 @@ public class FuelRateData {
         }
 
         return matchVal;
+    }
+    
+    public Boolean getValidData(){
+        return validData;
     }
 
     public double getFuelUsage() {
